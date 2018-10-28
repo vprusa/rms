37bf0f3c0fc6d4050a23918459e2710f588447ad
Martin Lacko <433769@mail.muni.cz>
2018-10-28 18:00:49 +0100

    Added docstring for User and UserType
    Signed-off-by: Martin Lacko <433769@mail.muni.cz>

--- src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/enums/UserType.java
+++ src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/enums/UserType.java
@@ -1,5 +1,17 @@
 package cz.muni.fi.pa165.skupina06.team02.rms.app.enums;
 
+/**
+ * Types of user
+ *
+ * @author Martin Lacko
+ */
 public enum UserType {
-    Tenant, Lesee
+    /**
+     * Basic Tennant
+     */
+    Tenant,
+    /**
+     * Landlord or managing tennant
+     */
+    Lesee
 }
