82d88fc1400857d0ef2a070b9548086bd37da856
Martin Lacko <433769@mail.muni.cz>
2018-10-28 17:15:56 +0100

    Created User class
    Signed-off-by: Martin Lacko <433769@mail.muni.cz>

---
+++ src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/entity/User.java
@@ -0,0 +1,66 @@
+package cz.muni.fi.pa165.skupina06.team02.rms.app.entity;
+
+import javax.validation.constraints.NotNull;
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotNull
+    @Column(nullable = false)
+    private String lastName;
+
+    @NotNull
+    @Column(nullable = false)
+    private String passwordHash;
+
+//  TODO: After implementing:
+//      - add households property
+//      - add items to buy
+
+    public User(Long userID) {
+        this.id = userID;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
