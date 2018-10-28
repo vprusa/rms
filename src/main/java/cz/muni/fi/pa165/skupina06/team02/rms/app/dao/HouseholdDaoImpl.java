4065dae20fa03d3228520364f2785fb57060507a
Vojtěch Průša <prusa.vojtech@gmail.com>
2018-10-28 22:20:13 +0100

    First DAO sketch

---
+++ src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/dao/HouseholdDaoImpl.java
@@ -0,0 +1,48 @@
+package cz.muni.fi.pa165.skupina06.team02.rms.app.dao;
+
+import java.util.List;
+
+import cz.muni.fi.pa165.skupina06.team02.rms.app.entity.Household;
+
+/**
+ * @author Vojtech Prusa
+ *
+ */
+public class HouseholdDaoImpl implements HouseholdDao {
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.HouseholdDao#findById(java.lang.Long)
+     */
+    @Override
+    public Household findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.HouseholdDao#create(cz.muni.fi.pa165.skupina06.team02.rms.app.entity.Household)
+     */
+    @Override
+    public void create(Household h) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.HouseholdDao#delete(cz.muni.fi.pa165.skupina06.team02.rms.app.entity.Household)
+     */
+    @Override
+    public void delete(Household h) {
+        // TODO Auto-generated method stub
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.HouseholdDao#findAll()
+     */
+    @Override
+    public List<Household> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/dao/ShoppingListDaoImpl.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.pa165.skupina06.team02.rms.app.dao;
+
+import java.util.List;
+
+import cz.muni.fi.pa165.skupina06.team02.rms.app.entity.ShoppingList;
+
+/**
+ * @author Vojtech Prusa
+ *
+ */
+public class ShoppingListDaoImpl implements ShoppingListDao {
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.ShoppingListDao#findById(java.lang.Long)
+     */
+    @Override
+    public ShoppingListDao findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.ShoppingListDao#create(cz.muni.fi.pa165.skupina06.team02.rms.app.entity.ShoppingList)
+     */
+    @Override
+    public void create(ShoppingList sl) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.ShoppingListDao#delete(cz.muni.fi.pa165.skupina06.team02.rms.app.entity.ShoppingList)
+     */
+    @Override
+    public void delete(ShoppingList sl) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.ShoppingListDao#findAll()
+     */
+    @Override
+    public List<ShoppingList> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.dao.ShoppingListDao#findByName(java.lang.String)
+     */
+    @Override
+    public ShoppingListDao findByName(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
