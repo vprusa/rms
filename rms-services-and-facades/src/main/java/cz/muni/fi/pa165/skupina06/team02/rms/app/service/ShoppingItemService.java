390aa2b098a26578668771633956861fd22fef44
Martin Lacko <433769@mail.muni.cz>
2018-11-25 12:39:39 +0100

    Added comments about the difference about move/add
    Signed-off-by: Martin Lacko <433769@mail.muni.cz>

--- rms-services-and-facades/src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/service/ShoppingItemService.java
+++ rms-services-and-facades/src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/service/ShoppingItemService.java
@@ -19,6 +19,7 @@ public interface ShoppingItemService {
 
     /**
      * Find shopping item by its ID
+     *
      * @param id ID of the shopping item to find
      * @return item with matching ID, null if such item does not exists
      */
@@ -26,6 +27,7 @@ public interface ShoppingItemService {
 
     /**
      * Find shopping items by dedicated buyer
+     *
      * @param user dedicated buyer
      * @return list of items which should dedicated buyer buy
      */
@@ -33,6 +35,7 @@ public interface ShoppingItemService {
 
     /**
      * Find all items on shopping list
+     *
      * @param shoppingList shopping list with items
      * @return list of item on given shopping list
      */
@@ -45,13 +48,15 @@ public interface ShoppingItemService {
 
     /**
      * Set buyer for and shopping item
+     *
      * @param shoppingItem Shopping item to set the buyer
-     * @param user dedicated buyer for item
+     * @param user         dedicated buyer for item
      */
     void setDedicatedBuyer(ShoppingItem shoppingItem, User user);
 
     /**
      * Update shopping item
+     *
      * @param shoppingItem instance to update
      */
     void updateShoppingItem(ShoppingItem shoppingItem);
@@ -63,6 +68,9 @@ public interface ShoppingItemService {
 
     /**
      * Add item to the specific shopping list
+     * <p>
+     * If shopping item has already set the shopping list, InvalidArgumentException is thrown.
+     *
      * @param shoppingItem item to add to the shopping list
      * @param shoppingList shopping list to add the item
      */
@@ -70,6 +78,9 @@ public interface ShoppingItemService {
 
     /**
      * Move item to other shopping list
+     * <p>
+     * Moves item to list, without checking, if list is not already set.
+     *
      * @param shoppingItem item to move
      * @param shoppingList shoppinglist to move the item
      */
