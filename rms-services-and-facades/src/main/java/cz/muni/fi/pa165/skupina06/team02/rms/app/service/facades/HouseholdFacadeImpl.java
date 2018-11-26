197695269bee23f8492d7403bef228ac8ecb1292
Votjtěch Průša <prusa.vojtech@gmail.com>
2018-11-26 22:28:59 +0100

    Fixes, additional methods

--- rms-services-and-facades/src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/service/facades/HouseholdFacadeImpl.java
+++ rms-services-and-facades/src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/service/facades/HouseholdFacadeImpl.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO;
+import cz.muni.fi.pa165.skupina06.team02.rms.app.dto.UserDTO;
 import cz.muni.fi.pa165.skupina06.team02.rms.app.entity.Household;
+import cz.muni.fi.pa165.skupina06.team02.rms.app.entity.User;
 import cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade;
 import cz.muni.fi.pa165.skupina06.team02.rms.app.service.BeanMappingService;
 import cz.muni.fi.pa165.skupina06.team02.rms.app.service.HouseholdService;
-
+import cz.muni.fi.pa165.skupina06.team02.rms.app.service.UserService;
 
 /**
  * @author Vojtech Prusa
  *
- * Household Facade
+ *         Household Facade
  *
  */
 @Service
@@ -26,19 +28,28 @@ public class HouseholdFacadeImpl implements HouseholdFacade {
     private HouseholdService householdService;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private BeanMappingService beanMappingService;
 
-    /* (non-Javadoc)
-     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#getHouseholdById(java.lang.Long)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#
+     * getHouseholdById(java.lang.Long)
      */
     @Override
-    public HouseholdDTO getHouseholdById(Long id) {
+    public HouseholdDTO findHouseholdById(Long id) {
         Household household = householdService.findHouseholdById(id);
         return (household == null) ? null : beanMappingService.mapTo(household, HouseholdDTO.class);
     }
 
-    /* (non-Javadoc)
-     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#createHousehold(cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#
+     * createHousehold(cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO)
      */
     @Override
     public Long createHousehold(HouseholdDTO householdDTO) {
@@ -48,11 +59,77 @@ public class HouseholdFacadeImpl implements HouseholdFacade {
         return household.getId();
     }
 
-    /* (non-Javadoc)
-     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#getAllHouseholds()
+    /*
+     * (non-Javadoc)
+     * 
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#
+     * getAllHouseholds()
      */
     @Override
-    public List<HouseholdDTO> getAllHouseholds() {
+    public List<HouseholdDTO> findAll() {
         return beanMappingService.mapTo(householdService.findAll(), HouseholdDTO.class);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#
+     * getHouseholdsByUser(cz.muni.fi.pa165.skupina06.team02.rms.app.dto.UserDTO)
+     */
+    /*
+    @Override
+    public List<HouseholdDTO> getHouseholdsByUser(UserDTO user) {
+        List<Household> houseolds =  householdService.getHouseholdsByUser(beanMappingService.mapTo(user, User.class));-
+        return beanMappingService.mapTo(houseolds, HouseholdDTO.class);
+    }*/
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#
+     * updateHousehold(cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO)
+     */
+    @Override
+    public void updateHousehold(HouseholdDTO household) {
+        householdService.updateHousehold(beanMappingService.mapTo(household, Household.class));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#
+     * deleteHousehold(cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO)
+     */
+    @Override
+    public void deleteHousehold(HouseholdDTO household) {
+        householdService.deleteHousehold(beanMappingService.mapTo(household, Household.class));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#removeTenant
+     * (cz.muni.fi.pa165.skupina06.team02.rms.app.dto.UserDTO,
+     * cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO)
+     */
+    @Override
+    public void removeTenant(UserDTO tenant, HouseholdDTO household) {
+        householdService.removeTenant(beanMappingService.mapTo(tenant, User.class),
+                beanMappingService.mapTo(household, Household.class));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * cz.muni.fi.pa165.skupina06.team02.rms.app.facade.HouseholdFacade#addTenant(cz
+     * .muni.fi.pa165.skupina06.team02.rms.app.dto.UserDTO,
+     * cz.muni.fi.pa165.skupina06.team02.rms.app.dto.HouseholdDTO)
+     */
+    @Override
+    public void addTenant(UserDTO tenant, HouseholdDTO household) {
+        householdService.addTenant(beanMappingService.mapTo(tenant, User.class),
+                beanMappingService.mapTo(household, Household.class));
     }
 }
