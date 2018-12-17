a055bff21be91cc21194501ab74e56fca15e31da
Votjtěch Průša <prusa.vojtech@gmail.com>
2018-12-17 20:50:15 +0100

    Fixe

--- rms-web/src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/web/rest/controllers/BaseController.java
+++ rms-web/src/main/java/cz/muni/fi/pa165/skupina06/team02/rms/app/web/rest/controllers/BaseController.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pa165.skupina06.team02.rms.app.web.rest.controllers;
 
+import cz.muni.fi.pa165.skupina06.team02.rms.app.web.rest.ApiUris;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("rest")
+@RequestMapping(ApiUris.API_PREFIX)
 public abstract class BaseController {}
